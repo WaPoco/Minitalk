3a24e20e6f5055e2cd3a374eb0eb6957b4fe886b
WaPoco1994 <vasili-pogorelov@web.de>
2025-02-11 13:24:23 +0100

    Normed a bit of the code

--- client.c
+++ client.c
@@ -6,42 +6,50 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-int received = 0;
+int	received = 0;
 
-void signal_received()
+void	signal_received()
 {
-    received = 1;
+	received = 1;
 }
 
-static void sendMessage(pid_t pid, int digit)
+static void	sendMessage(pid_t pid, int digit)
 {
-    int i = 7;
-    while (i >= 0)
-    {
-        received = 0; 
-        if ((digit >> i & 1) == 0)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        if (received == 0)
-            usleep(500);
-        i--;
-    }
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		received = 0;
+		if ((digit >> i & 1) == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		if (received == 0)
+			pause();
+		i--;
+	}
 }
 
 int main(int args0, char **args) 
 {
-    (void)args0;
-    pid_t pid = atoi(args[1]);
-    char *message = args[2];
-    int len = ft_strlen(message);
-    signal(SIGUSR1, signal_received);
-    int i = 0;
-    while (i < len)
-    {
-        sendMessage(pid, message[i]);
-        i++;
-    }
-    sendMessage(pid, 0);
-    return (0);
+	int		i;
+	int		len;
+	pid_t	pid;
+	char	*message;
+
+    if (args0 - 1 != 2 || !args[1] || !args[2])
+	{
+		ft_printf("Error arguments\n");
+		return (0);
+	}
+	pid = ft_atoi(args[1]);
+	message = ft_atoi(args[2]);
+	len = ft_strlen(message);
+	i = 0;
+	signal(SIGUSR1, signal_received);
+	while (i < len)
+		sendMessage(pid, message[i++]);
+	sendMessage(pid, 0);
+	return (0);
 }
