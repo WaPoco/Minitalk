2573e7c1e1d820c7f155712c7292423ec31ec958
WaPoco1994 <vasili-pogorelov@web.de>
2025-02-12 19:36:56 +0100

    Passes normi

--- client.c
+++ client.c
@@ -1,55 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpogorel <vpogorel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 18:30:29 by vpogorel          #+#    #+#             */
+/*   Updated: 2025/02/12 19:34:15 by vpogorel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include "libft.h"
 #include "ft_printf.h"
 
-int	received = 0;
+int	g_received;
 
-void	signal_received()
+void	signal_received(int t)
 {
-	received = 1;
+	(void)t;
+	g_received = 1;
 }
 
-static void	sendMessage(pid_t pid, int digit)
+static void	send_message(pid_t pid, int digit)
 {
 	int	i;
 
-	i = 7;
-	while (i >= 0)
+	i = 0;
+	while (i < 8)
 	{
-		received = 0;
+		g_received = 0;
 		if ((digit >> i & 1) == 0)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		if (received == 0)
+		if (g_received == 0)
 			pause();
-		i--;
+		i++;
 	}
 }
 
-int main(int args0, char **args) 
+int	main(int args0, char **args)
 {
 	int		i;
 	int		len;
 	pid_t	pid;
 	char	*message;
 
-    if (args0 - 1 != 2 || !args[1] || !args[2])
+	if (args0 - 1 != 2 || !args[1] || !args[2])
 	{
 		ft_printf("Error arguments\n");
 		return (0);
 	}
 	pid = ft_atoi(args[1]);
-	message = ft_atoi(args[2]);
+	message = args[2];
 	len = ft_strlen(message);
 	i = 0;
+	g_received = 0;
 	signal(SIGUSR1, signal_received);
 	while (i < len)
-		sendMessage(pid, message[i++]);
-	sendMessage(pid, 0);
+		send_message(pid, message[i++]);
+	send_message(pid, 0);
 	return (0);
 }
--- server.c
+++ server.c
@@ -1,98 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpogorel <vpogorel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 18:19:48 by vpogorel          #+#    #+#             */
+/*   Updated: 2025/02/12 19:32:40 by vpogorel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include "libft.h"
 #include "ft_printf.h"
 
-char *text = NULL;
+char	*g_text;
 
-char *re_alloc(char *str, int new_size)
+char	*re_alloc(char *str, int new_size)
 {
-  int len;
-  char *new_str;
-  int i;
+	int		len;
+	int		i;
+	char	*new_str;
 
-  i = 0;
-  len = ft_strlen(str);
-  new_str = malloc(new_size);
-  while (i < len)
-  {
-    new_str[i] = str[i];
-    i++; 
-  }
-  free(str);
-  while (i < new_size)
-  {
-    new_str[i] = '\0';
-    i++; 
-  }
-  return (new_str);
+	i = 0;
+	len = ft_strlen(str);
+	new_str = malloc(new_size);
+	while (i < len)
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	free(str);
+	while (i < new_size)
+		new_str[i++] = '\0';
+	return (new_str);
 }
 
-void signal_handler(int signum, siginfo_t *info, void *context) 
+void	resize(int c_letter, int *k, int *letter, int *text_len)
 {
-  (void)context;
-  static int k = 7;
-  static int c_letter;
-  static int letter;
-  pid_t client_pid = 0;
-  char *new_text;
-  static int text_len;
+	char	*new_text;
 
-  if (info->si_pid != 0)
-    client_pid = info->si_pid;
-  if (signum == 12)
-    letter = (1 << k) | letter;
-  k--;
-  if (k < 0)
-  {
-    c_letter++;
-    if (letter == 0)
-    {
-      kill(client_pid, SIGUSR1);
-      if (c_letter != 1)
-        ft_printf("%s\n", text);
-      free(text);
-      text = NULL;
-      c_letter = 0;
-      k = 7;
-      text_len = 0;
-      return ;
-    }
-    if (c_letter >= text_len)
-    {
-      if (text_len == 0)
-        text_len++;
-      new_text = re_alloc(text, 2 * text_len);
-      text_len *= 2; 
-      text = new_text;
-    }
-    text[c_letter - 1] = (char)letter; 
-    letter = 0;
-    k = 7;
-  }
-  if (client_pid != 0)
-    kill(client_pid, SIGUSR1);
+	if (c_letter >= *text_len)
+	{
+		if (*text_len == 0)
+			(*text_len)++;
+		new_text = re_alloc(g_text, 2 * (*text_len));
+		(*text_len) *= 2;
+		g_text = new_text;
+	}
+	g_text[c_letter - 1] = (char)(*letter);
+	*letter = 0;
+	*k = 0;
 }
 
-int main(void) 
+void	end_of_message(int *letter, int *k)
 {
-  struct sigaction action;
-  action.sa_flags = SA_SIGINFO;
-  sigemptyset(&action.sa_mask);
-  action.sa_sigaction = signal_handler;
-  /*
-  signal(SIGUSR1, signal_handler);
-  signal(SIGUSR2, signal_handler);
-  */
-  sigaction(SIGUSR1, &action, NULL);
-  sigaction(SIGUSR2, &action, NULL);
-  printf("%d\n", getpid());
-  printf("Process started. PID: %d\n", getpid());
-  while (1) {
-    //pause();
-  }
-  return (0);
+	static int	c_letter[1];
+	static int	text_len[1];
+
+	c_letter[0]++;
+	if (*letter == 0)
+	{
+		if (c_letter[0] != 1)
+			ft_printf("%s\n", g_text);
+		free(g_text);
+		g_text = NULL;
+		c_letter[0] = 0;
+		*k = 0;
+		text_len[0] = 0;
+		return ;
+	}
+	resize(*c_letter, k, letter, text_len);
+}
+
+void	signal_handler(int signum, siginfo_t *info, void *context)
+{
+	static int	k;
+	static int	letter;
+	pid_t		client_pid;
+
+	(void)context;
+	client_pid = 0;
+	if (info->si_pid != 0)
+		client_pid = info->si_pid;
+	if (signum == 12)
+		letter = (1 << k) | letter;
+	k++;
+	if (k == 8)
+		end_of_message(&letter, &k);
+	if (client_pid != 0)
+		kill(client_pid, SIGUSR1);
+}
+
+int	main(void)
+{
+	struct sigaction	action;
+
+	g_text = NULL;
+	action.sa_flags = SA_SIGINFO;
+	sigemptyset(&action.sa_mask);
+	action.sa_sigaction = signal_handler;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
+	printf("%d\n", getpid());
+	printf("Process started. PID: %d\n", getpid());
+	while (1)
+	{
+	}
+	return (0);
 }
